e1014a0cc361281ade39d449347335422ab0f167
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2024-12-28 01:17:07 -0300

    Botão de voltar para o topo; corrigido e aparece somente após rolagem de 200px

--- components/VoltarAoTopo.tsx
+++ components/VoltarAoTopo.tsx
@@ -1,15 +1,31 @@
-"use client"
-import { ChevronUpIcon } from 'lucide-react'
-import React from 'react'
+"use client";
+import { ChevronUpIcon } from "lucide-react";
+import React, { useState, useEffect } from "react";
 
 const VoltarAoTopo = () => {
-  return (
-        <div>
-            <button className='fixed bottom-4 left-4 z-50 bg-white rounded-full shadow-lg border border-black/5' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} >
-                <ChevronUpIcon className="text-black w-14 h-14" />
-            </button>
-        </div>
-  )
-}
+  const [isVisible, setIsVisible] = useState(false);
 
-export default VoltarAoTopo
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 200); // Mostra o botão ao rolar mais de 200px
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  if (!isVisible) return null; // Não renderiza o botão se não estiver visível
+
+  return (
+    <div>
+      <button
+        className="fixed bottom-4 left-4 z-50 bg-white rounded-full shadow-lg border border-black/5"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        <ChevronUpIcon className="text-black w-14 h-14" />
+      </button>
+    </div>
+  );
+};
+
+export default VoltarAoTopo;
