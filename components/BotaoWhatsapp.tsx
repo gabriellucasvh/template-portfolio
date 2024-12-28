6e5c20c531e2a120eec9c73d039d9dc203e04433
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2024-12-28 01:20:00 -0300

    Animação para o botão de whatsapp

--- components/BotaoWhatsapp.tsx
+++ components/BotaoWhatsapp.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const BotaoWhatsapp = () => {
     return (
         <div>
-            <button className='fixed bottom-4 right-4 z-50  shadow-lg rounded-full shadow-black/35'>
-                <Image src="/whatsapp.svg" alt="whatsapp" width={50} height={50} />
+            <button className='fixed bottom-4 right-4 z-50  shadow-lg rounded-full shadow-black/35 animate-bounce'>
+                <Image src="/whatsapp.svg"  alt="whatsapp" width={50} height={50} />
             </button>
         </div>
     )
